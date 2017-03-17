631bef1777f793319c8bfa92c7e8da0a041179fe
Koichiro Asai <koichiro.asai@hds.com>
2017-03-17 12:21:51 -0700

    checkpoint-9.5-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
+ 
      var revealPoint = function(x) {
          points[x].style.opacity = 1;
          points[x].style.transform = "scaleX(1) translateY(0)";
@@ -14,3 +14,11 @@ var animatePoints = function() {
      }
  
 };
+
+ window.onload = function() {
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     
+     window.addEventListener('scroll', function(event) {
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+     });     
+ };
